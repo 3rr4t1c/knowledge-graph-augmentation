7728ddeb7957c181587579279c59620facba157e
Enrico Verdolotti <enrico.verdolotti@gmail.com>
2021-01-28 15:06:33 +0100

    Miglioramento prestazioni
    Migliorate notevolmente le prestazioni per file di grandi dimensioni ed aggiunta la modalità di funzionamento senza tipi delle entità.

--- main/selector.py
+++ main/selector.py
@@ -13,8 +13,9 @@ import random
 
 class SELector:
     
-    def __init__(self):
+    def __init__(self, notypes=False):
         self.model_state = 'NOT READY'
+        self.notypes = notypes # Booleano: True per non considerare i tipi delle entità
         self.text_triples = []
         self.knowledge_graph = []
         self.labeled_triples = []
@@ -32,64 +33,69 @@ class SELector:
 
     # Effettua il distant supervision
     def distant_supervision(self):
-        for txt, e1, t1, e2, t2 in self.text_triples:
-            triple = None
-            for h, r, t in self.knowledge_graph:                
-                if h == e1 and t == e2:
-                    triple = (txt, e1, t1, e2, t2, r)
+        # Versione improponibile con costo O(tt.size x kg.size)
+        # for txt, e1, t1, e2, t2 in self.text_triples:
+        #     triple = None
+        #     for h, r, t in self.knowledge_graph:                
+        #         if h == e1 and t == e2:
+        #             triple = (txt, e1, t1, e2, t2, r)
+        #             self.labeled_triples.append(triple)
+        #     if triple == None:
+        #         triple = (txt, e1, t1, e2, t2, 'unknown')
+        #         self.unlabeled_triples.append(triple)
+        
+        # Versione ottimizzata con costo O(tt.size + kg.size) :)
+        kg_dict = dict() # TODO: il kg può essere caricato direttamente in questa struttura
+        # Carica kg in struttura ottimizzata per ricerca
+        for h, r, t in self.knowledge_graph:
+            try:
+                kg_dict[(h,t)].append(r)
+            except:
+                kg_dict[(h,t)] = [r]
+        # Iterazione a singolo ciclo for e accesso diretto
+        for phr, e1, t1, e2, t2 in self.text_triples:
+            try:
+                for rel in kg_dict[(e1,e2)]:
+                    triple = (phr, e1, t1, e2, t2, rel)
+                    if self.notypes:
+                        triple = (phr, e1, '', e2, '', rel)
                     self.labeled_triples.append(triple)
-            if triple == None:
-                triple = (txt, e1, t1, e2, t2, 'unknown')
+            except:
+                triple = (phr, e1, t1, e2, t2, 'unknown')
+                if self.notypes:
+                    triple = (phr, e1, '', e2, '', 'unknown')
                 self.unlabeled_triples.append(triple)
-        # In effetti mantenere gli id delle entità non servirebbe per la fase successiva...
+        # In effetti mantenere gli id delle entità non servirebbe per la fase successiva ma...
         # ...solo se la pipeline fosse quella originale, volendo usare Link Prediction gli id sono necessari. 
 
 
-    # Addestra il modello, la funzione di match deve sempre restituire un bool
-    def build_model_triples(self, match_function=lambda x, y: x == y):
-        # Unisci tutte le triple ed assegna un id incrementale
-        all_triples = self.labeled_triples + self.unlabeled_triples        
-        # Group by match
-        while all_triples:
-            # Ottiene tutta la lista tranne l'elemento affiorante
-            tail_triples = all_triples[1:]
-            # Rimuove l'elemento affiorante dalla lista all_triples
-            head_triple = all_triples.pop(0)
-            htxt, _, ht1, _, ht2, hrel = head_triple
-            # Relazione: numero di occorrenze per la phrase corrente
-            rel2count = {hrel: 1}
-            # Itera sul resto della lista
-            for triple in tail_triples:
-                txt, _, t1, _, t2, rel = triple
-                # Solo se i tipi corrispondono esattamente
-                if t1 == ht1 and t2 == ht2:
-                    # Caso1: match esatto della Phrase (originale)
-                    if txt == htxt:
-                        # Incrementa il conteggio per la relazione associata
-                        # e rimuove la tripla corrente da all_triples
-                        try:
-                            rel2count[rel] += 1
-                        except:
-                            rel2count[rel] = 1                                                
-                        all_triples.remove(triple)                         
-                    # Caso2: match inesatto della Phrase (sperimentale, disattivato di default)
-                    elif match_function(txt, htxt):
-                        # Incrementa il conteggio per la relazione associata 
-                        # la tripla viene lasciata nella lista per successive iterazioni
-                        try:
-                            rel2count[rel] += 1
-                        except:
-                            rel2count[rel] = 1
-            # Estrazione della relazione con conteggio maggiore ed aggiornamento modello
-            max_rel = max(rel2count, key=rel2count.get)
-            if max_rel != 'unknown':
-                self.model_triples.append((htxt, ht1, ht2, max_rel, rel2count[max_rel]))
-            # TODO: Gestire il caso di conteggi "pari merito" con unknown, risultato non deterministico
-
-        self.model_triples.sort(key=lambda t: t[4], reverse=True) # Sort descending by count
+    # Costruisce la tabella necessaria per estrarre fatti
+    def build_model_triples(self):
+        # Dizionario da labeled e unlabeled {(phr, t1, t2) -> {rel1: count, rel1: count}}
+        all_triples = self.labeled_triples + self.unlabeled_triples
+        pattern2relc = dict()
+        for phr, _, t1, _, t2, rel in all_triples:
+            try:
+                counts = pattern2relc[(phr, t1, t2)]
+                try:
+                    counts[rel] += 1
+                except:
+                    counts[rel] = 1
+            except:
+                pattern2relc[(phr, t1, t2)] = {rel: 1}
+        # Max val key su ciascuna chiave per assegnare relazione
+        for pattern, counts in pattern2relc.items():
+            phr, t1, t2 = pattern
+            rel = max(counts, key=counts.get) # Qui viene scelta la relazione con "max count" da associare al pattern
+            if rel != 'unknown':
+                triple = (phr, t1, t2, rel, counts[rel])
+                self.model_triples.append(triple)
+        
+        # Ordina pattern per occorrenza decrescente
+        self.model_triples.sort(key=lambda t: t[4], reverse=True)
 
     
-    # Addestra il modello con Distant supervision
+    # Addestramento modello
     def train(self, input_text_triples, input_knowledge_graph, subsamp=1, rseed=42):        
         # Se viene passato un path carica da file altrimenti assegna
         if type(input_text_triples) is str:
@@ -103,15 +109,21 @@ class SELector:
             self.knowledge_graph = input_knowledge_graph
         
         # Distant Supervision
-        self.distant_supervision()        
+        print('Generando training set con distant supervision...', end='', flush=True)
+        self.distant_supervision()
+        print('Fatto.', flush=True)        
         
         # Sottocampionamento unknown o LP
+        print('Sottocampionamento casuale delle triple non etichettate...', end='', flush=True)
         num_sample = int(len(self.unlabeled_triples) * subsamp)
         random.seed(rseed) # Deterministico, usa rseed=None per seed casuali
         self.unlabeled_triples = random.sample(self.unlabeled_triples, k=num_sample)
+        print('Fatto.', flush=True)
         
         # Costruisce la tabella che serve per le predizioni
+        print('Generando le triple del modello per le estrazioni...', end='', flush=True)
         self.build_model_triples()
+        print('Fatto.', flush=True)
         
         # Elimina le strutture ausiliarie
         #del(self.text_triples)
@@ -122,46 +134,53 @@ class SELector:
         self.model_state = 'READY'
 
 
-    # Singola predizione di un fatto a partire da una tripla estratta dal testo
-    def predict(self, text_triple, match_function=lambda x, y: x == y):
-        assert self.model_state == 'READY'
-        txt, e1, t1, e2, t2 = text_triple
-        inexact_matches = dict()
-        for mtxt, mt1, mt2, mr, _ in self.model_triples:                
-            if mt1 == t1 and mt2 == t2:
-                # Caso 1: Match esatto (originale)
-                if mtxt == txt:
-                    return (e1, mr, e2)       
-                # Caso 2: Match inesatto (sperimentale, disattivato di default)
-                elif match_function(mtxt, txt):
-                    try:
-                        inexact_matches[mr] += 1
-                    except:
-                        inexact_matches[mr] = 1
+    # Costruisce un dizionario pattern -> relazione per match rapidi
+    def build_mt_map(self):
+        mt_map = dict()
+        for phr, t1, t2, r, c in self.model_triples:
+            mt_map[(phr, t1, t2)] = (r, c)
         
-        if inexact_matches:
-            max_rel = max(inexact_matches, key=inexact_matches.get)            
-            return (e1, max_rel, e2)
+        return mt_map
+  
 
-        # Restituisce unknown se falliscono tutti i controlli precedenti
-        return (e1, 'unknown', e2)   # NOTA: questo tipo di fatto è utile in certi casi
-
-
-    # Estrazione fatti, i fatti nella forma (e1, unknown, e2) vengono scartati
-    def harvest(self, input_text_triples, match_function=lambda x, y: x == y):
+    # Singola predizione di un fatto a partire da una tripla estratta dal testo
+    def predict(self, text_triple, use_map=None):
+        # Controllo stato del modello
         assert self.model_state == 'READY'
+        # Spacchetta tripla
+        phr, e1, t1, e2, t2 = text_triple
+        # Controllo se modalità senza tipi è attiva
+        if self.notypes:
+            t1, t2 = '', ''
+        # Controllo se struttura dati per match passata
+        if use_map:
+            model_triples_map = use_map
+        else:
+            model_triples_map = self.build_mt_map()
+        # Match ad accesso diretto (corrispondenza esatta del pattern)
+        try:            
+            rel, _ = model_triples_map[(phr, t1, t2)]
+        except:
+            rel = 'unknown'
 
+        return (e1, rel, e2)
+
+
+    # Estrazione fatti, N.B. i fatti nella forma (e1, unknown, e2) non vengono prodotti
+    def harvest(self, input_text_triples):
+        # Controllo stato del modello
+        assert self.model_state == 'READY'
         # Se passi un path carica da tsv altrimenti usa il riferimento
         if type(input_text_triples) is str:
             text_triples = list()
             self.load_tsv(input_text_triples, text_triples)
         else:
             text_triples = input_text_triples
-
         # Iterazione su ogni tripla estratta dal testo
+        mt_map = self.build_mt_map()
         result = list()
         for triple in text_triples:
-            fact = self.predict(triple, match_function)
+            fact = self.predict(triple, mt_map)
             if fact[1] != 'unknown':
                 result.append(fact)
 
@@ -170,26 +189,25 @@ class SELector:
 
     # Valuta le prestazioni del modello, necessita di una ground truth
     def evaluate(self, input_text_triples, input_ground_truth):
+        # Controllo stato del modello
         assert self.model_state == 'READY'
-
-        # Carica da disco se passi un path
+        # Carica da disco se passi un path (text triples)
         if type(input_text_triples) == str:
             text_triples = list()
             self.load_tsv(input_text_triples, text_triples)
         else:
             text_triples = input_text_triples
-
-        # Carica da disco se passi un path
+        # Carica da disco se passi un path (knowledge graph)
         if type(input_ground_truth) == str:
             ground_truth = list()
             self.load_tsv(input_ground_truth, ground_truth)
         else:
             ground_truth = input_ground_truth
-
-        # Effettua le predizioni con il modello
+        # Effettua le predizioni con il modello e valuta prestazioni
+        mt_map = self.build_mt_map()
         pred_card, true_card, relevant_card = 0, 0, 0        
         for i, txt_triple in enumerate(text_triples):
-            _, relation, _ = self.predict(txt_triple)
+            _, relation, _ = self.predict(txt_triple, mt_map)
             gt_relation = ground_truth[i][0]
             # Se il fatto predetto non è unknown
             if relation != 'unknown':
@@ -202,11 +220,19 @@ class SELector:
             # Conta le relazioni rilevanti nella ground truth
             if gt_relation != 'unknown':
                 relevant_card += 1
+        # Calcolo precision, recall ed fScore
+        precision, recall, fscore = 0, 0, 0        
+        try:
+            precision = true_card / pred_card
+            recall = true_card / relevant_card
+            fscore =(precision*recall/(precision+recall))*2
+        except:
+            print('\nDivisione per 0, qualcosa non va bene:')
+            print(f'Fatti estratti totali: {pred_card}')
+            print(f'Fatti estratti corretti: {true_card}')
+            print(f'Fatti rilevanti totali: {relevant_card}')
 
-        precision = true_card / pred_card
-        recall = true_card / relevant_card
-
-        return precision, recall
+        return precision, recall, fscore
 
 
     # Salva il modello (model_triples)
@@ -218,10 +244,12 @@ class SELector:
     # Utility, mostra il contenuto di una struttura dati
     def show_list(self, struct_name, outer=None, limit=100):
         print(f'\nShowing {struct_name}:')
-        if outer:
-            struct = outer
+        # Verifica se struttura interna al modello o esterna
+        if outer != None:
+            struct = outer                        
         else:
-            struct = getattr(self, struct_name)
+            struct = getattr(self, struct_name)            
+        # Stampa struttura in modo ordinato  
         for line in struct[:limit]:
             print(line)
     
@@ -230,11 +258,18 @@ class SELector:
 ## TEST AREA ## Esegui da shell per avviare questa demo
 if __name__ == '__main__':
 
-    sel = SELector()
-    sel.train(f'input_data{sep}text_triples.tsv', 
-              f'input_data{sep}knowledge_graph.tsv', 
-              subsamp=0.8)
+    # File esempio giocattolo
+    toy_tt = f'input_data/toy_example/text_triples.tsv'
+    toy_kg = f'input_data/toy_example/knowledge_graph.tsv'
 
+    # Files esempio con dati reali estratti da Lector
+    mini_lector_kg = 'input_data/lector_example/knowledge_graph.tsv'
+    mini_lector_tt = 'input_data/lector_example/text_triples.tsv'
+
+    sel = SELector(notypes=True) # Modello inizializzato in modalità senza controllo dei tipi
+    sel.train(toy_tt, toy_kg, subsamp=0.8) # Sottocampionamento delle unlabeled a 0.8
+
+    # Mostra strutture dati dopo l'addestramento
     sel.show_list('text_triples')
     
     sel.show_list('knowledge_graph')
@@ -245,10 +280,14 @@ if __name__ == '__main__':
     
     sel.show_list('model_triples')
 
-    sel.show_list('harvested_triples', sel.harvest(sel.text_triples))
+    # Test estrazione fatti
+    harvested = sel.harvest('input_data/toy_example/text_triples.tsv')
+    sel.show_list('harvested_triples', harvested)
 
-    precision, recall = sel.evaluate(sel.text_triples, f'input_data{sep}text_triples_gt.tsv')
-    print('\ntest sui dati di training')
+    # Valutazione modello con ground truth
+    precision, recall, fscore = sel.evaluate('input_data/toy_example/text_triples.tsv', 
+                                             'input_data/toy_example/text_triples_gt.tsv')
+    print('\nValutazione sulle text triples usate per il training:')
     print(f'Precision: {precision}')
     print(f'Recall: {recall}')
-    print(f'fScore: {(precision * recall / (precision + recall)) * 2} ')
+    print(f'fScore: {fscore}')
