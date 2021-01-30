522ed06a2d4f27d2b627c13000feb649d73f46f7
Enrico Verdolotti <enrico.verdolotti@gmail.com>
2021-01-30 18:37:03 +0100

    Modifiche ai commenti del codice

--- main/selector.py
+++ main/selector.py
@@ -1,6 +1,6 @@
 # Selection Enhanced Lector, SELector (by Enrico Verdolotti)
-# Questo progetto mira a migliorare le prestazioni di un progetto preesistente: Lector
-# Tutta la pipeline è stata reimplementata, i metodi esposti che si consiglia di usare sono:
+# Questo modulo definisce l'oggetto SELector, un modello per l'estrazione di relazioni.
+# I metodi esposti che si consiglia di usare sono:
 # train(...) addestra il modello
 # predict(...) predice un singolo fatto
 # harvest(...) estrazione di tutti i fatti
@@ -15,8 +15,13 @@ import csv
 class SELector:
     
     def __init__(self, notypes=False):
+        # Stato modello
         self.model_state = 'NOT READY'
-        self.notypes = notypes # Booleano: True per non considerare i tipi delle entità
+        # Iperparametri
+        self.notypes = notypes 
+        # random seed
+        # unlabeled subsampling
+        # Strutture dati 
         self.text_triples = []
         self.knowledge_graph = []
         self.labeled_triples = []
@@ -31,31 +36,12 @@ class SELector:
             rd = csv.reader(tsv_file, delimiter='\t')
             for line in rd:                
                 dest.append(tuple(line))
-        
-
-        #OLD CODE problemi con i campi in cui c'è solo lo spazio
-        # with open(file_path, 'r') as tsv_file:
-        #     for line in tsv_file:                               
-        #         field = tuple(field for field in line.strip().split(sep='\t'))
-        #         dest.append(field)
                 
 
-    # Effettua il distant supervision
+    # Genera le tabelle Labeled e Unlabeled Triples con Distant Supervision
     def distant_supervision(self):
-        # Versione improponibile con costo O(tt.size x kg.size)
-        # for txt, e1, t1, e2, t2 in self.text_triples:
-        #     triple = None
-        #     for h, r, t in self.knowledge_graph:                
-        #         if h == e1 and t == e2:
-        #             triple = (txt, e1, t1, e2, t2, r)
-        #             self.labeled_triples.append(triple)
-        #     if triple == None:
-        #         triple = (txt, e1, t1, e2, t2, 'unknown')
-        #         self.unlabeled_triples.append(triple)
-        
-        # Versione ottimizzata con costo O(tt.size + kg.size) :)
-        kg_dict = dict() # TODO: il kg può essere caricato direttamente in questa struttura
         # Carica kg in struttura ottimizzata per ricerca
+        kg_dict = dict() # NOTA: il KG potrebbe essere caricato direttamente in questa struttura        
         for h, r, t in self.knowledge_graph:
             try:
                 kg_dict[(h,t)].append(r)
@@ -98,14 +84,14 @@ class SELector:
             rel = max(counts, key=counts.get) # Qui viene scelta la relazione con "max count" da associare al pattern
             if rel != 'unknown':
                 triple = (phr, t1, t2, rel, counts[rel])
-                self.model_triples.append(triple)
-        
+                self.model_triples.append(triple)        
         # Ordina pattern per occorrenza decrescente
         self.model_triples.sort(key=lambda t: t[4], reverse=True)
 
     
     # Addestramento modello
-    def train(self, input_text_triples, input_knowledge_graph, subsamp=1, rseed=42):        
+    def train(self, input_text_triples, input_knowledge_graph, subsamp=1, rseed=42):
+
         # Se viene passato un path carica da file altrimenti assegna
         if type(input_text_triples) is str:
             print('Caricamento tabella text triples in corso...', end='', flush=True)
@@ -113,6 +99,7 @@ class SELector:
             print('Fatto.', flush=True)
         else:
             self.text_triples = input_text_triples
+
         # Se viene passato un path carica da file altrimenti assegna
         if type(input_knowledge_graph) is str:
             print('Caricamento knowledge graph in corso...', end='', flush=True)
@@ -120,19 +107,19 @@ class SELector:
             print('Fatto.', flush=True)
         else:
             self.knowledge_graph = input_knowledge_graph
-        
-        # Distant Supervision
+
+        # Distant Supervision (INSERIRE LINK PREDICTION QUI oppure...)
         print('Generazione training set con distant supervision...', end='', flush=True)
         self.distant_supervision()
-        print('Fatto.', flush=True)        
-        
-        # Sottocampionamento unknown o LP
+        print('Fatto.', flush=True)
+
+        # Sottocampionamento unlabeled triples (...INSERIRE LINK PREDICTION QUI)
         print('Sottocampionamento casuale delle triple non etichettate...', end='', flush=True)
         num_sample = int(len(self.unlabeled_triples) * subsamp)
         random.seed(rseed) # Deterministico, usa rseed=None per seed casuali
         self.unlabeled_triples = random.sample(self.unlabeled_triples, k=num_sample)
         print('Fatto.', flush=True)
-        
+
         # Costruisce la tabella che serve per le predizioni
         print('Generando le triple del modello per le estrazioni...', end='', flush=True)
         self.build_model_triples()
@@ -182,14 +169,17 @@ class SELector:
     # Estrazione di fatti da text_triples, keep_unknown = True 
     # genererà anche fatti con relazione 'unknown'
     def harvest(self, input_text_triples, keep_unknown=False):
+
         # Controllo stato del modello
         assert self.model_state == 'READY'
+
         # Se passi un path carica da tsv altrimenti usa il riferimento
         if type(input_text_triples) is str:
             text_triples = list()
             self.load_tsv(input_text_triples, text_triples)
         else:
             text_triples = input_text_triples
+
         # Iterazione su ogni tripla estratta dal testo
         mt_map = self.build_mt_map()
         result = list()
@@ -203,20 +193,24 @@ class SELector:
 
     # Valuta le prestazioni del modello, necessita di una ground truth
     def evaluate(self, input_text_triples, input_ground_truth):
+
         # Controllo stato del modello
         assert self.model_state == 'READY'
+
         # Carica da disco se passi un path (text triples)
         if type(input_text_triples) == str:
             text_triples = list()
             self.load_tsv(input_text_triples, text_triples)
         else:
             text_triples = input_text_triples
+
         # Carica da disco se passi un path (knowledge graph)
         if type(input_ground_truth) == str:
             ground_truth = list()
             self.load_tsv(input_ground_truth, ground_truth)
         else:
             ground_truth = input_ground_truth
+
         # Effettua le predizioni con il modello e valuta prestazioni
         mt_map = self.build_mt_map()
         pred_card, true_card, relevant_card = 0, 0, 0        
@@ -234,6 +228,7 @@ class SELector:
             # Conta le relazioni rilevanti nella ground truth
             if gt_relation != 'unknown':
                 relevant_card += 1
+
         # Calcolo precision, recall ed fScore
         precision, recall, fscore = 0, 0, 0        
         try:
@@ -251,18 +246,22 @@ class SELector:
 
     # Salva il modello (model_triples)
     def save_to_tsv(self, save_path):
+
         # TODO: Da implementare salvataggio su file tsv di model_triples
         print('Not implemented yet.')
 
 
     # Utility, mostra il contenuto di una struttura dati
     def show_list(self, struct_name, outer=None, limit=100):
+
         print(f'\nShowing {struct_name}:')
+
         # Verifica se struttura interna al modello o esterna
         if outer != None:
             struct = outer                        
         else:
-            struct = getattr(self, struct_name)            
+            struct = getattr(self, struct_name)
+                        
         # Stampa struttura in modo ordinato  
         for line in struct[:limit]:
             print(line)
